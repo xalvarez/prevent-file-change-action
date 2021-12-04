e44bd18c61733a5d6c22067e26b8f67d509653ca
Xavier Alvarez <xalvarez@users.noreply.github.com>
2021-12-04 21:00:10 +0100

    Add missing 'await'

--- src/pattern-matcher.ts
+++ src/pattern-matcher.ts
@@ -6,12 +6,12 @@ export default class PatternMatcher {
     if (files?.length > 0) {
       const regExp = new RegExp(pattern)
       files.some(file => regExp.test(file.filename))
-        ? this.setFailed(pattern)
+        ? await PatternMatcher.setFailed(pattern)
         : core.debug(`There isn't any file matching the pattern ${pattern}`)
     } else core.debug(`This commit doesn't contain any files`)
   }
 
-  private async setFailed(pattern: string): Promise<void> {
+  private static async setFailed(pattern: string): Promise<void> {
     core.setFailed(`There is at least one file matching the pattern ${pattern}`)
     return
   }
